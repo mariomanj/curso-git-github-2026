a9104e33dcad5c0500363de457e618af9193f295
mariomanj <mariomanj@hotmail.com>
2026-06-15 21:15:59 -0300

    feat: ola mundo em python

---
+++ ola_mundo.py
@@ -0,0 +1,3 @@
+print("Olá, Mundo!")
+# Este é um programa simples que imprime "Olá, Mundo!" na tela.
+# Ele é frequentemente usado como um exemplo introdutório para aprender a programar em uma nova linguagem. O código é escrito em Python, uma linguagem de programação popular e fácil de aprender. Ao executar este programa, você verá a mensagem "Olá, Mundo!" exibida no console.
